8dcb70b4022777a89aaf58fa78525013fbc90894
Liang, Jeffrey H <jeffrey.liang@uhc.com>
2024-01-17 13:29:45 -0800

    Update with progress bar.

---
+++ ExcelLauncher.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQT_Launcher
+{
+    internal class ExcelLauncher
+    {
+        const string DatabaseName = "WN000053767";
+        const string InitialCatalog = "UMR";
+        const string InitialSchema = "SALES";
+        const string InitialTable = "SQT_PARAMETERS";
+        const string LookupField_SqtUrl = "SQT_URL";
+        const string LookupField_SqtFilename = "SQT_FILENAME";
+        const string LookupField_SqtVersion = "SQT_VERSION";
+        const string LookupField_ProposalTemplateUrl = "PROPOSAL_TEMPLATE_URL";
+        const string LookupField_ProposalTemplateFilename = "PROPOSAL_TEMPLATE_FILENAME";
+        const int ConnectTimeout = 300;
+
+        public ExcelLauncher()
+        {
+            InitSqlConnection();
+            LookupSqtVersion();
+            LookupSqtUrl();
+        }
+        private SqlConnection Sc;
+        public void InitSqlConnection()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = DatabaseName;
+            builder.InitialCatalog = InitialCatalog;
+            builder.IntegratedSecurity = true;  // true == use Windows Authentication
+            builder.ConnectTimeout = ConnectTimeout;    // time limit on queries 
+            builder.MultipleActiveResultSets = true;
+            Sc = new SqlConnection(builder.ConnectionString);
+            Sc.Open();
+        }
+
+        public void OpenSqt(string networkPath, string localTempPath)
+        {
+
+            var uri = new Uri(networkPath);
+            var fName = Path.GetFullPath(localTempPath);
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile(uri, fName);
+            }
+        }
+        public string LookupSqtVersion()
+        {
+            // Look up from sql
+            return RetrieveStringFromSql(LookupField_SqtVersion);
+        }
+        public string LookupSqtUrl()
+        {
+            return RetrieveStringFromSql(LookupField_SqtUrl);
+        }
+        public string LookupSqtFilename()
+        {
+            return RetrieveStringFromSql(LookupField_SqtFilename);
+        }
+        public string LookupProposalTemplateUrl()
+        {
+            return RetrieveStringFromSql(LookupField_ProposalTemplateUrl);
+        }
+        public string LookupProposalTemplateFilename()
+        {
+            return RetrieveStringFromSql(LookupField_ProposalTemplateFilename);
+        }
+
+        private string RetrieveStringFromSql(string field)
+        {
+            string retrievedString = null;
+            string query = "SELECT " + field + " FROM " + InitialSchema + "." + InitialTable + " ORDER BY ID DESC";
+            StringBuilder errorMessages = new StringBuilder();
+            try
+            {
+                if (Sc.State == System.Data.ConnectionState.Closed)
+                {
+                    Sc.Open();
+                }
+                using (SqlCommand command = new SqlCommand(query, Sc))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (!reader.IsDBNull(0)) { retrievedString = reader.GetString(0); break; }
+                        }
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                for (int i = 0; i < ex.Errors.Count; i++)
+                {
+                    errorMessages.Append("Index #" + i + "\n" +
+                        "Message: " + ex.Errors[i].Message + "\n" +
+                        "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
+                        "Source: " + ex.Errors[i].Source + "\n" +
+                        "Procedure: " + ex.Errors[i].Procedure + "\n");
+                }
+                Console.WriteLine(errorMessages.ToString());
+            }
+            //Console.WriteLine(field + ": " + retrievedString);
+            return retrievedString;
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -15,27 +15,32 @@ namespace SQT_Launcher
 {
     internal class Program
     {
-        //static async Task Main(string[] args)
         const int versionNumber = 1;
-        
+
         static void Main(string[] args)
         {
             string user = Environment.UserName;
 
+            /*
+            // LAUNCH INSTALLER
             Process p = new Process();
-            p.StartInfo.Arguments = @"""C:\Users\jliang12\Documents\2023\UMR (local)\Sales Tool\Installer\installer dev 1.vbs""";
-            p.StartInfo.FileName = @"C:\Windows\System32\cscript.exe";
+            p.StartInfo.FileName = @"""C:\Users\jliang12\Documents\2023\UMR (local)\Sales Tool\Installer\installer dev 1.vbs""";
             p.StartInfo.Arguments = versionNumber.ToString();   
 
             p.Start();
             p.WaitForExit();
             p.Close();
+            */
 
+            Console.WriteLine("Welcome to the Sales Quote Tool (SQT). Please wait while the file is loaded.\n");
+
+            ProgressUpdate("Updating local SQT directory...", 1);
             ExcelLauncher launcher = new ExcelLauncher();
-
             string localDir = @"C:\Users\" + user + @"\Documents\2023\SQT\";
             Directory.CreateDirectory(localDir);
+            Console.WriteLine(" Done.");
 
+            ProgressUpdate("Retrieving SQT network parameters...", 1);
             string sqtFilename = launcher.LookupSqtFilename();
             string localPathSqt = localDir + sqtFilename;
             string sqtUrl = launcher.LookupSqtUrl();
@@ -43,25 +48,37 @@ namespace SQT_Launcher
             string proposalTemplateFilename = launcher.LookupProposalTemplateFilename();
             string localPathProposalTemplate = localDir + proposalTemplateFilename;
             string proposalTemplateUrl = launcher.LookupProposalTemplateUrl();
+            Console.WriteLine(" Done.");
 
+            ProgressUpdate("Retrieving the latest version of SQT...", 1);
             Application excel = new Application();
 
             using (WebClient wc = new WebClient())
             {
                 wc.DownloadFile(sqtUrl, localPathSqt);
+                wc.DownloadFile(proposalTemplateUrl, localPathProposalTemplate);
             }
+            Console.WriteLine(" Done.");
 
+            ProgressUpdate("Launching Excel...", 1);
             Workbook sqt = excel.Workbooks.Open(localPathSqt);
             sqt.BeforeSave += new WorkbookEvents_BeforeSaveEventHandler(ThisWorkbook_BeforeSave);
             excel.DisplayAlerts = false;
             excel.Visible = true;
+            Console.WriteLine(" Done.");
+
+            Console.WriteLine();
+            Console.WriteLine("SQT is currently in use by " + user + ". Please do not close this window.");
+            Console.WriteLine();
 
             FileInfo f = new FileInfo(localPathSqt);
             while (IsFileLocked(f))
             {
                 Thread.Sleep(1000);
             }
+            ProgressUpdate("Cleaning up local files...", 1);
             DeleteFile(localPathSqt);
+            Console.WriteLine(" Done.");
         }
 
         static void ThisWorkbook_BeforeSave(bool SaveAsUI, ref bool Cancel)
@@ -112,109 +129,22 @@ namespace SQT_Launcher
             //file is not locked
             return false;
         }
-
+        static void ProgressUpdate(string action, int threadTimer)
+        {
+            Console.Write(action);
+            using (var pb = new ProgressBar())
+            {
+                //var pb = new ProgressBar();
+                for (int i = 0; i <= 100; i++)
+                {
+                    pb.Report((double)i / 100);
+                    Thread.Sleep(threadTimer);
+                }
+            }
+            //Console.WriteLine("\b");
+            //}
+            //Console.WriteLine(" Done.");
+        }
     }
 
-    internal class ExcelLauncher
-    {
-        const string DatabaseName = "WN000053767";
-        const string InitialCatalog = "UMR";
-        const string InitialSchema = "SALES";
-        const string InitialTable = "SQT_PARAMETERS";
-        const string LookupField_SqtUrl = "SQT_URL";
-        const string LookupField_SqtFilename = "SQT_FILENAME";
-        const string LookupField_SqtVersion = "SQT_VERSION";
-        const string LookupField_ProposalTemplateUrl = "PROPOSAL_TEMPLATE_URL";
-        const string LookupField_ProposalTemplateFilename = "PROPOSAL_TEMPLATE_FILENAME";
-        const int ConnectTimeout = 300;
-
-        public ExcelLauncher() {
-            InitSqlConnection();
-            LookupSqtVersion();
-            LookupSqtUrl();
-        }
-        private SqlConnection Sc;
-        public void InitSqlConnection()
-        {
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = DatabaseName;
-            builder.InitialCatalog = InitialCatalog;
-            builder.IntegratedSecurity = true;  // true == use Windows Authentication
-            builder.ConnectTimeout = ConnectTimeout;    // time limit on queries 
-            builder.MultipleActiveResultSets = true;
-            Sc = new SqlConnection(builder.ConnectionString);
-            Sc.Open();
-        }
-
-        public void OpenSqt(string networkPath, string localTempPath)
-        {
-            
-            var uri = new Uri(networkPath);
-            var fName = Path.GetFullPath(localTempPath);
-            using (WebClient wc = new WebClient())
-            {
-                wc.DownloadFile(uri, fName);
-            }
-        }
-        public string LookupSqtVersion() 
-        {
-            // Look up from sql
-            return RetrieveStringFromSql(LookupField_SqtVersion); 
-        }
-        public string LookupSqtUrl()
-        {
-            return RetrieveStringFromSql(LookupField_SqtUrl);
-        }
-        public string LookupSqtFilename()
-        {
-            return RetrieveStringFromSql(LookupField_SqtFilename);
-        }
-        public string LookupProposalTemplateUrl()
-        {
-            return RetrieveStringFromSql(LookupField_ProposalTemplateUrl);
-        }
-        public string LookupProposalTemplateFilename()
-        {
-            return RetrieveStringFromSql(LookupField_ProposalTemplateUrl);
-        }
-
-        private string RetrieveStringFromSql(string field)
-        {
-            string retrievedString = null;
-            string query = "SELECT " + field + " FROM " + InitialSchema + "." + InitialTable + " ORDER BY ID DESC";
-            StringBuilder errorMessages = new StringBuilder();
-            try
-            {
-                if (Sc.State == System.Data.ConnectionState.Closed)
-                {
-                    Sc.Open();
-                }
-                using (SqlCommand command = new SqlCommand(query, Sc))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            if (!reader.IsDBNull(0)) { retrievedString = reader.GetString(0); break; }
-                        }
-                    }
-
-                }
-            } catch (SqlException ex)
-            {
-                for (int i = 0; i < ex.Errors.Count; i++)
-                {
-                    errorMessages.Append("Index #" + i + "\n" +
-                        "Message: " + ex.Errors[i].Message + "\n" +
-                        "LineNumber: " + ex.Errors[i].LineNumber + "\n" +
-                        "Source: " + ex.Errors[i].Source + "\n" +
-                        "Procedure: " + ex.Errors[i].Procedure + "\n");
-                }
-                Console.WriteLine(errorMessages.ToString());
-            }
-            //Console.WriteLine(field + ": " + retrievedString);
-            return retrievedString;
-        }
-
-    }
 }
